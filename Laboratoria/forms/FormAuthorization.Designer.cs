b1d92433b2f651531f8700eef56c0a95cc1c07b6
Denis Kardakov <kardakov.denis@mail.ru>
2022-06-15 13:35:53 +0300

    Добавьте файлы проекта.

---
+++ Laboratoria/forms/FormAuthorization.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Laboratoria
+{
+    partial class FormAuthorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAuthorization));
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.checkBoxShowPassword = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLogin.Location = new System.Drawing.Point(25, 167);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(40, 15);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Логин";
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLogin.Location = new System.Drawing.Point(28, 184);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(150, 23);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPassword.Location = new System.Drawing.Point(25, 223);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(48, 15);
+            this.labelPassword.TabIndex = 0;
+            this.labelPassword.Text = "Пароль";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPassword.Location = new System.Drawing.Point(28, 240);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(150, 23);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = global::Laboratoria.Properties.Resources.logo;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(28, 12);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(150, 132);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.TabIndex = 1;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(227)))), ((int)(((byte)(131)))));
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonLogin.Font = new System.Drawing.Font("Comic Sans MS", 10.25F);
+            this.buttonLogin.Location = new System.Drawing.Point(28, 303);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(150, 42);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "Войти";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // checkBoxShowPassword
+            // 
+            this.checkBoxShowPassword.AutoSize = true;
+            this.checkBoxShowPassword.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            this.checkBoxShowPassword.Location = new System.Drawing.Point(64, 269);
+            this.checkBoxShowPassword.Name = "checkBoxShowPassword";
+            this.checkBoxShowPassword.Size = new System.Drawing.Size(116, 19);
+            this.checkBoxShowPassword.TabIndex = 3;
+            this.checkBoxShowPassword.Text = "Показать пароль";
+            this.checkBoxShowPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBoxShowPassword.UseVisualStyleBackColor = true;
+            this.checkBoxShowPassword.CheckedChanged += new System.EventHandler(this.checkBoxShowPassword_CheckedChanged);
+            // 
+            // FormAuthorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(204, 361);
+            this.Controls.Add(this.checkBoxShowPassword);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(220, 400);
+            this.MinimumSize = new System.Drawing.Size(220, 400);
+            this.Name = "FormAuthorization";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.CheckBox checkBoxShowPassword;
+    }
+}
+
---
+++ Laboratoria/forms/FormAuthorization.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Laboratoria
+{
+    public struct User
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public int type { get; set; }
+        public string photo { get; set; }
+    }
+    public partial class FormAuthorization : Form
+    {
+        public FormAuthorization()
+        {
+            InitializeComponent();
+        }
+
+        public static User user = new User();
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text == "" && textBoxPassword.Text == "")
+            {
+                MessageBox.Show("Введите данные!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                bool key = false;
+                foreach (users _user in Program.LaboratoriaDB.users)
+                {
+                    if (textBoxLogin.Text == _user.login && textBoxPassword.Text == _user.password)
+                    {
+                        key = true;
+                        user.id = _user.id;
+                        user.login = _user.login;
+                        user.password = _user.password;
+                        user.firstName = _user.firstName;
+                        user.lastName = _user.lastName;
+                        user.type = _user.type;
+                        user.photo = _user.photo;
+                    }
+                }
+                if (!key)
+                {
+                    MessageBox.Show("Проверьте данные", "Пользователь не найден", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    //FormMenu menu = new FormMenu(this);
+                    //menu.Show();
+                    //Hide();
+                    //textBoxPassword.Text = "";
+                    MessageBox.Show("данные найдены!", "Всё ок", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    foreach (var prop in typeof(User).GetProperties())
+                    {
+                        Console.WriteLine($"{prop.Name} {prop.GetValue(user)}");
+                    }
+                }
+            }
+        }
+
+        private void checkBoxShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxPassword.UseSystemPasswordChar = !checkBoxShowPassword.Checked;
+        }
+    }
+}
