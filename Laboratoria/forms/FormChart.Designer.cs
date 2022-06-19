31e39cb3192ef663cb5d278c1247cbba7685cecc
Denis Kardakov <kardakov.denis@mail.ru>
2022-06-19 13:57:23 +0300

    Chart

---
+++ Laboratoria/forms/FormChart.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Laboratoria.forms
+{
+    partial class FormChart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormChart));
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBoxChart = new System.Windows.Forms.GroupBox();
+            this.groupBoxFields = new System.Windows.Forms.GroupBox();
+            this.groupBoxButtons = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.groupBoxChart.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.LabelStyle.Angle = -90;
+            chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            chartArea1.AxisX.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Rotated270;
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea1.AxisX2.IsLabelAutoFit = false;
+            chartArea1.AxisX2.LabelStyle.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea1.AxisX2.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Rotated270;
+            chartArea1.AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Rotated90;
+            chartArea1.Name = "ChartArea";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Cursor = System.Windows.Forms.Cursors.Default;
+            legend1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "LegendServicesPrice";
+            legend1.Title = "Легенда";
+            legend1.TitleFont = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(6, 22);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.ChartArea = "ChartArea";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            series1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series1.Legend = "LegendServicesPrice";
+            series1.LegendText = "Цена, руб";
+            series1.MarkerSize = 1;
+            series1.Name = "ServicesPrice";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(666, 466);
+            this.chart.TabIndex = 0;
+            // 
+            // groupBoxChart
+            // 
+            this.groupBoxChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxChart.Controls.Add(this.chart);
+            this.groupBoxChart.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxChart.Name = "groupBoxChart";
+            this.groupBoxChart.Size = new System.Drawing.Size(678, 494);
+            this.groupBoxChart.TabIndex = 1;
+            this.groupBoxChart.TabStop = false;
+            this.groupBoxChart.Text = "График";
+            // 
+            // groupBoxFields
+            // 
+            this.groupBoxFields.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxFields.Location = new System.Drawing.Point(698, 13);
+            this.groupBoxFields.Name = "groupBoxFields";
+            this.groupBoxFields.Size = new System.Drawing.Size(223, 259);
+            this.groupBoxFields.TabIndex = 2;
+            this.groupBoxFields.TabStop = false;
+            this.groupBoxFields.Text = "Поля";
+            // 
+            // groupBoxButtons
+            // 
+            this.groupBoxButtons.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxButtons.Location = new System.Drawing.Point(698, 278);
+            this.groupBoxButtons.Name = "groupBoxButtons";
+            this.groupBoxButtons.Size = new System.Drawing.Size(223, 223);
+            this.groupBoxButtons.TabIndex = 2;
+            this.groupBoxButtons.TabStop = false;
+            this.groupBoxButtons.Text = "Действия";
+            // 
+            // FormChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.groupBoxButtons);
+            this.Controls.Add(this.groupBoxFields);
+            this.Controls.Add(this.groupBoxChart);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(949, 558);
+            this.Name = "FormChart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "График";
+            this.Load += new System.EventHandler(this.FormChart_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.groupBoxChart.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.GroupBox groupBoxChart;
+        private System.Windows.Forms.GroupBox groupBoxFields;
+        private System.Windows.Forms.GroupBox groupBoxButtons;
+    }
+}
---
+++ Laboratoria/forms/FormChart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Laboratoria.forms
+{
+    public partial class FormChart : Form
+    {
+        public FormChart()
+        {
+            InitializeComponent();
+        }
+
+        private void FormChart_Load(object sender, EventArgs e)
+        {
+            chart.Series[0].Points.Clear();
+            foreach (service _service in Program.LaboratoriaDB.service)
+            {
+                chart.Series[0].Points.AddXY(_service.Service,_service.Price);
+            }
+        }
+    }
+}
